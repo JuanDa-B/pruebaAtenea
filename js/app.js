f89e0b6e22390e35e5c29d3b3eb3ae7972e06ab0
JuanDa-B <80796796+JuanDa-B@users.noreply.github.com>
2023-04-24 21:16:27 -0500

    dsada

--- js/app.js
+++ js/app.js
@@ -5,10 +5,7 @@ $(document).ready(function(){
     var screen = $('#loading-screen');
     configureLoadingScreen(screen);
 
-    $('.do-request').on('click', function(){
-         $.get('http://jsonplaceholder.typicode.com/posts')
-             .done(function(result){
-                 $('#results').text(JSON.stringify(result, null, 2))
+    
              })
              .fail(function(error{
                  console.error(error)
